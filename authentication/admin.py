c650eef5e2e61e92bd08a1add7d4d3136004eaf4
Mehedi Hasan <mehedi4595@gmail.com>
2023-11-29 13:40:32 +0600

    User Type Create or User Model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-admin.site.register(CustomUser)
+class UserModel(UserAdmin):
+    list_display = ['username', 'user_type']
+
+admin.site.register(CustomUser, UserModel)
