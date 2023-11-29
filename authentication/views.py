2f5674d4ef0c11eac398f4a90694ccdfc7922bc6
Mehedi Hasan <mehedi4595@gmail.com>
2023-11-29 20:14:12 +0600

    Login With User Authentication

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, HttpResponse
+from .EmailBackEnd import EmailBackEnd
+from django.contrib.auth import authenticate, logout, login
 # Create your views here.
 def loginPage(request):
+    if request.method == 'POST':
+        user = EmailBackEnd.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        if user!=None:
+            login(request, user)
+            user_type = user.user_type
+            if user_type == '1':
+                return HttpResponse('This is Admin Panel')
+            elif user_type == '2':
+                return HttpResponse('This is Teacher Panel')
+            elif user_type == '3':
+                return HttpResponse('This is Student Panel')
+            else:
+                return redirect('loginPage')
+        else:
+            return redirect('loginPage')
     return render(request, 'login.html')
 
 def signupPage(request):
