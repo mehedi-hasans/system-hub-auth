b0ee76c34a82b1de5d11be75c33f7d41089d86cb
Mehedi Hasan <mehedi4595@gmail.com>
2023-11-30 15:48:32 +0600

    Create Student Model

--- authentication/models.py
+++ authentication/models.py
@@ -11,3 +11,35 @@ class CustomUser(AbstractUser):
 
     user_type = models.CharField(choices=USER, max_length=50, default=1)
     profile_pic = models.ImageField(upload_to='media/profile_pic')
+
+
+
+#Course Create
+
+class Course(models.Model):
+    name= models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class SessionYear(models.Model):
+    session_start = models.CharField(max_length=100)
+    session_end = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.session_start + '-' + self.session_end
+    
+
+class Student(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    address=models.TextField()
+    gender = models.CharField(max_length=100)
+    course_id = models.ForeignKey(Course, on_delete=models.DO_NOTHING,default=1)  # Set the default course
+    sessionyearid = models.ForeignKey(SessionYear, on_delete=models.DO_NOTHING,default=1)
+    cratedat = models.DateTimeField(auto_now_add=True)  # Set the default value using timezone.now
+    updateat = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.admin.first_name + " " + self.admin.last_name
--- authentication/views.py
+++ authentication/views.py
@@ -114,4 +114,3 @@ def changePassword(request):
 
 
 
-
