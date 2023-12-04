2ffa3ff23ff0c549b5584225d0ba7c34a6adeaf7
Mehedi Hasan <mehedi4595@gmail.com>
2023-12-04 11:54:31 +0600

    customized

--- authentication/views.py
+++ authentication/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login as auth_login
 #Profile Update
 from .models import *
 # Create your views here.
+
 def loginPage(request):
     if request.method == 'POST':
         user = EmailBackEnd.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
@@ -28,7 +29,25 @@ def loginPage(request):
     return render(request, 'login.html')
 
 def signupPage(request):
-    return render(request, 'signup.html')
+    error_messages = {
+        'password_error': 'Password and Confirm Password not match',
+    }
+    if request.method == "POST":
+        uname = request.POST.get("name")
+        email = request.POST.get("email")
+        pass1 = request.POST.get("password")
+        pass2 = request.POST.get("confirmpassword")
+
+        if pass1!= pass2:
+             messages.error(request, error_messages['password_error'])
+        else:
+            # Use your customUser model to create a user
+            myuser = CustomUser.objects.create_user(username=uname, email=email, password=pass1)
+            myuser.save()
+            return redirect("loginPage")
+
+    messages.success(request, 'Signup successful.')
+    return render(request, "signup.html")
 
 @login_required(login_url='/')
 def index(request):
